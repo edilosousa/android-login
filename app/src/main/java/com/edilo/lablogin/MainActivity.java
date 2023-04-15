f31c7541c9cb3515fc35a88af0f5928a17e87101
Edilo <edilo13@gmail.com>
2023-04-14 20:31:43 -0400

    Cadastrar usuarios

--- app/src/main/java/com/edilo/lablogin/MainActivity.java
+++ app/src/main/java/com/edilo/lablogin/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        gotoRegister.findViewById(R.id.lognow);
+        gotoRegister = findViewById(R.id.registernow);
 
         gotoRegister.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/edilo/lablogin/Register.java
+++ app/src/main/java/com/edilo/lablogin/Register.java
@@ -1,34 +1,56 @@
 package com.edilo.lablogin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Register extends AppCompatActivity {
 
     EditText txtEmail, txtPass;
     MaterialButton btnReg;
     TextView  gotoLogin;
-
+    FirebaseAuth mAuth;
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_register);
         txtEmail = findViewById(R.id.edemail);
         txtPass = findViewById(R.id.edpass);
         btnReg = findViewById(R.id.btnentrar);
         gotoLogin = findViewById(R.id.lognow);
+        progressBar = findViewById(R.id.progressbar);
+
+        gotoLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
         btnReg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
                 String email,passwd;
 
                 email = txtEmail.getText().toString();
@@ -42,6 +64,27 @@ public class Register extends AppCompatActivity {
                     Toast.makeText(Register.this,"A senha nao pode estar vazia!",Toast.LENGTH_SHORT).show();
                     return;
                 }
+
+                mAuth.createUserWithEmailAndPassword(email, passwd)
+                        .addOnCompleteListener(Register.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    progressBar.setVisibility(View.GONE);
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d("sucesso", "createUserWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                } else {
+                                    progressBar.setVisibility(View.GONE);
+                                    // If sign in fails, display a message to the user.
+                                    Log.w("Failed", "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(Register.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+
             }
         });
     }
